6cc6a1beb57bbbe47b2ff5cf2bd8722f0b71a0cb
Danilqqqqqqqqqq <easy.ban@inbox.ru>
2026-04-02 13:26:35 +0300

    Add files via upload

---
+++ qweqweqwe(2).py
@@ -0,0 +1,11 @@
+
+import math
+
+x1 = float(input("Введите x1: "))
+y1 = float(input("Введите y1: "))
+x2 = float(input("Введите x2: "))
+y2 = float(input("Введите y2: "))
+
+qweqweqew = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+print("Расстояние между точками р1авно:", qweqweqew)
